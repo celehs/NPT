3fcf16e5e2d38a8a6b076b645dcc7e7c01ec72a8
Ming Yang <hustyangming@gmail.com>
2020-01-17 03:43:08 +0000

    update

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' Non-Parametric Transformation (NPT) Model
+#'
+#' Functions written in C++ to fit non-parametric transformation (NPT) model.
+#'
+#' @docType package
+#' @name NPT-package
+#' @aliases NPT
+#' @keywords package
+#' @useDynLib NPT
+#' @import Rcpp RcppArmadillo
+NULL
